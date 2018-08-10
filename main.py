baecb25bb381aada7acf15cac27e9392e01cbd75
Chansung <deep.diver.csp@gmail.com>
2018-08-10 13:09:35 +0900

    make single_gpu_convnet, multi_gpu_convnet. - multi_gpu_convnet is under coding yet.

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
 
 # activation function is not specified yet
-def main():
-    # hyper-parameters
-    leraning_rate = 0.1
-    
+def single_gpu_convnet():
     input = tf.placeholder(tf.float32, [None, 224, 224, 3])
 
     conv1 = tf.nn.conv2d(input, [11, 11, 3, 96], [1, 4, 4, 1], "SAME")
@@ -24,9 +21,18 @@ def main():
     fcl1 = tf.contrib.layers.fully_connected(flat, 4096)
     fcl2 = tf.contrib.layers.fully_connected(fcl1, 4096)
     out = tf.contrib.layers.fully_connected(fcl2, 1000, activation_fn=None)
-    
+
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out, labels=y))
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+def multi_gpu_convnet():
+    
+
+def main():
+    # hyper-parameters
+    leraning_rate = 0.1
+
+
 
     with tf.Session() as sess:
         sess.run(optimizer,
